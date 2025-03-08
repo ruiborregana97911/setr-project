57c1415574e3a9a2ab6dc2deee53969457b70ae6
Rui Borregana <ruiborregana@ua.pt>
2025-03-08 15:39:38 +0000

    funcao ecAdd v1

--- enerconsumption.c
+++ enerconsumption.c
@@ -14,11 +14,18 @@ void dtSum();
 int main(){
 	
 	float consumption[96];	//96= numero de intervalos em 24H
+	float peak, dailyValue, dailyCost;
 	 
 	ecInit(consumption);
 	
+	ecAdd(consumption);
+	ecAdd(consumption);
 	
 	
+		for(int i=0;i<96;i++){
+		
+			fprintf(stdout, "%f \n", consumption[i]);
+		}
 	
 	return 0;}
 
@@ -34,6 +41,28 @@ void ecInit(float vect[]){
 		}
 	
 	}	
+
+//adiciona um novo valor que e pedido ao utilizador
+void ecAdd(float vect[]){
+	
+	static int index=95;
+	float new_val=0;
+	
+	if(index >=96){
+		index=0;
+		}
+	
+	
+	fprintf(stdout, "insira um novo valor: ");
+	scanf("%f", &new_val);
+	
+	vect[index]= new_val;
+	
+	index++;
+	
+	}
+
+
 
 
 
