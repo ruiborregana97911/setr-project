79f8b1101eaea37ab49d71aa771068cd17e27f5a
Rui Borregana <ruiborregana@ua.pt>
2025-03-08 12:34:07 +0000

    funcao ecnInit v1

--- enerconsumption.c
+++ enerconsumption.c
@@ -6,18 +6,34 @@
 
 
 
-void ecInit();
+void ecInit(float vect[]);
 void ecAdd();
 void dtSum();
 
 
 int main(){
 	
+	float consumption[96];	//96= numero de intervalos em 24H
+	 
+	ecInit(consumption);
+	
+	
 	
 	
 	return 0;}
 
 
+// Funções :
+
+//inicia  todas a posicoes do array com o valor zero
+void ecInit(float vect[]){
+	
+	for(int i=0;i<96;i++){
+		
+		vect[i]=0;
+		}
+	
+	}	
 
 
 
