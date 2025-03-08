b72a1b613788a9b191fb0962d6f3cfc191d561c8
Rui Borregana <ruiborregana@ua.pt>
2025-03-08 11:27:01 +0000

    iniciação do ficheiro

---
+++ enerconsumption.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define COST_KWH 0.16
+
+
+
+void ecInit();
+void ecAdd();
+void dtSum();
+
+
+int main(){
+	
+	
+	
+	return 0;}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
